419a77fa7c46fb25edbe2b4a968ec213ebac938e
kr1shtal <kr1ssshtal@gmail.com>
2018-10-01 12:15:17 +0300

    Add float double and boolean

--- src/com/krishtal/serialization/SerializationWriter.java
+++ src/com/krishtal/serialization/SerializationWriter.java
@@ -15,6 +15,12 @@ public class SerializationWriter {
 		dest[pointer++] = (byte)((value >> 0) & 0xff);
 		return pointer;
 	}
+
+	public static int writeBytes(byte[] dest, int pointer, char value) {
+		dest[pointer++] = (byte)((value >> 8) & 0xff);
+		dest[pointer++] = (byte)((value >> 0) & 0xff);
+		return pointer;
+	}
 	
 	public static int writeBytes(byte[] dest, int pointer, int value) {
 		dest[pointer++] = (byte)((value >> 24) & 0xff);
@@ -33,6 +39,21 @@ public class SerializationWriter {
 		dest[pointer++] = (byte)((value >> 16) & 0xff);
 		dest[pointer++] = (byte)((value >> 8) & 0xff);
 		dest[pointer++] = (byte)((value >> 0) & 0xff);
+		return pointer;
+	}
+	
+	public static int writeBytes(byte[] dest, int pointer, float value) {
+		int data = Float.floatToIntBits(value);
+		return writeBytes(dest, pointer, data);
+	}
+	
+	public static int writeBytes(byte[] dest, int pointer, double value) {
+		long data = Double.doubleToLongBits(value);
+		return writeBytes(dest, pointer, data);
+	}
+	
+	public static int writeBytes(byte[] dest, int pointer, boolean value) {
+		dest[pointer++] = (byte)(value ? 1 : 0);
 		return pointer;
 	}
 	
