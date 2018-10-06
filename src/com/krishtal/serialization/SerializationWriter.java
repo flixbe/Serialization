8d01116e396b88f53e7a56332c3f895b3b479f93
krishtal <kr1ssshtal@gmail.com>
2018-10-06 19:10:43 +0300

    Reading values

--- src/com/krishtal/serialization/SerializationWriter.java
+++ src/com/krishtal/serialization/SerializationWriter.java
@@ -5,6 +5,12 @@ public class SerializationWriter {
 	public static final byte[] HEADER = "RC".getBytes();
 	public static final short VERSION = 0x0100;
 	
+	public static int writeBytes(byte[] dest, int pointer, byte[] source) {
+		for (int i = 0; i < source.length; i++)
+			dest[pointer++] = source[i];
+		return pointer;
+	}
+	
 	public static int writeBytes(byte[] dest, int pointer, byte value) {
 		dest[pointer++] = value;
 		return pointer;
@@ -55,6 +61,45 @@ public class SerializationWriter {
 	public static int writeBytes(byte[] dest, int pointer, boolean value) {
 		dest[pointer++] = (byte)(value ? 1 : 0);
 		return pointer;
+	}
+	
+	public static int writeBytes(byte[] dest, int pointer, String string) {
+		pointer = writeBytes(dest, pointer, (short) string.length());
+		return writeBytes(dest, pointer, string.getBytes());
+	}
+	
+	public static byte readByte(byte[] source, int pointer) {
+		return source[pointer];
+	}
+	
+	public static short readShort(byte[] source, int pointer) {
+		return (short)((source[pointer] << 8) | source[pointer + 1]);
+	}
+	
+	public static char readChar(byte[] source, int pointer) {
+		return (char)((source[pointer] << 8) | source[pointer + 1]);
+	}
+	
+	public static int readInt(byte[] source, int pointer) {
+		return (int)((source[pointer]) << 24 | (source[pointer + 1] << 16) | (source[pointer + 2] << 8) | source[pointer + 3]);
+	}
+	
+	public static long readLong(byte[] source, int pointer) {
+		return (long)((source[pointer]) << 56 | (source[pointer + 1] << 48) | (source[pointer + 2] << 40) | source[pointer + 3] << 32 | 
+					  (source[pointer + 4] << 24) | (source[pointer + 5] << 16) | (source[pointer + 6] << 8) | source[pointer + 7]);
+	}
+	
+	public static float readFloat(byte[] source, int pointer) {
+		return Float.intBitsToFloat(readInt(source, pointer));
+	}
+	
+	public static double readDouble(byte[] source, int pointer) {
+		return Double.longBitsToDouble(readLong(source, pointer));
+	}
+	
+	public static boolean readBoolean(byte[] source, int pointer) {
+		assert(source[pointer] == 0 || source[pointer] == 1);
+		return source[pointer] != 0;
 	}
 	
 }
